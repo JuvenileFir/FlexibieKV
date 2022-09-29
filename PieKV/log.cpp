1935cdff2cee430bd816eceaf37b4889eef6fe1e
antaresalice <antarescyan@outlook.com>
2022-09-29 13:27:51 +0800

    log functions implemention

---
+++ PieKV/log.cpp
@@ -0,0 +1,111 @@
+#include "log.hpp"
+
+
+LogSegment::LogSegment(/* args */)
+{
+}
+
+LogSegment::~LogSegment()
+{
+}
+
+Log::Log(/* args */)
+{
+}
+
+Log::~Log()
+{
+}
+
+/*
+expand log segemnt by `numBlockToExpand` blocks:
+`tableblocksToMove`: an array for ptrs of tableblocks to move
+`numBlockToExpand`: the number of blocks to move
+`blocksize`: init logblocks with this blocksize
+*/
+void Log::expand(TableBlock **tableblocksToMove, uint64_t numBlockToExpand, size_t blockSize)
+{
+    for(int i = 0; i < numBlockToExpand; i++) {
+        // first get the segment id to resize
+        uint16_t segmentId = get_next_resize_segment_id(0);
+        LogSegment *segmentToResize = logSegments[segmentId];
+        uint32_t blockNum = segmentToResize->blockNum;
+        segmentToResize->logBlocks[blockNum]->blockPtr = (uint8_t *)(tableblocksToMove[i]->blockPtr);
+        segmentToResize->logBlocks[blockNum]->blockId = tableblocksToMove[i]->blockId;
+        segmentToResize->logBlocks[blockNum]->Residue = blockSize;
+
+
+    }
+}
+
+void Log::shrink(uint64_t numBlockToShrink)
+{
+    for(int i = 0; i < numBlockToShrink; i++) {
+        // first get the segment id to resize
+        uint16_t segmentId = get_next_resize_segment_id(1);
+        LogSegment *segmentToResize = logSegments[segmentId];
+
+        // check if there is free block
+        if(segmentToResize->blockNum > 1 && segmentToResize->usingBlock < segmentToResize->blockNum - 1) {
+            __sync_fetch_and_sub((uint32_t *)&(segmentToResize->blockNum), 1U);
+            __sync_fetch_and_sub((uint16_t *)&(totalBlockNum), 1U);
+
+            // TODO: partitionMap_add removed here, should add it in Piekv::L2H
+        }
+        else {
+            numBlockToShrink++;         //  try to find next usable segment
+        }
+        set_next_resize_segment_id(segmentId);
+    }
+
+
+
+}
+
+ // expand: 0  shrink: 1
+uint16_t Log::get_next_resize_segment_id(int expandOrShrink) 
+{
+    if (expandOrShrink == 0) {
+        return resizingPointer;
+    }
+    if (expandOrShrink == 1) {
+        return (resizingPointer + totalSegmentNum - 1) % totalSegmentNum;
+    }
+}
+
+ // expand: 0  shrink: 1
+void Log::set_next_resize_segment_id(int expandOrShrink) 
+{
+    if (expandOrShrink == 0) {
+        resizingPointer = (resizingPointer + 1) % totalSegmentNum;
+    }
+    if (expandOrShrink == 1) {
+        resizingPointer = (resizingPointer + totalSegmentNum - 1) % totalSegmentNum;
+    }
+}
+
+
+int64_t LogSegment::allocItem(uint64_t item_size) {
+    // uint64_t item_size = mem_size;
+    //TODO: assert(item_size == ROUNDUP8(item_size));
+
+    int64_t item_offset;
+    if (item_size <= BATCH_SIZE) {
+        if (logBlocks[usingBlock]->Residue < item_size) {
+            // block in use is already filled up 
+            // check if there is free block left
+            if (usingBlock < blockNum - 1) {
+                // use next block
+                usingBlock++;
+                offset = 0;
+            }
+            else {
+                // no free block left
+                return -1;
+            }
+        }
+        else {
+            // TODO: implement a function `big_set`
+            return -2;  // batch_too_small
+        }
+}
--- PieKV/log.hpp
+++ PieKV/log.hpp
@@ -3,9 +3,11 @@
 
 #include <cstdint>
 #include "mempool.hpp"
+#include "hashtable.hpp"
 
 #define MAX 100  // a temp max num, remove it later
-
+#define BATCH_SIZE (2097152U)
+#define THREAD_NUM 4
 
 typedef struct StoreStats
 {
@@ -20,47 +22,55 @@ typedef struct TableStats
 
 typedef struct LogBlock
 {
-    uint8_t blockPtr;
+    uint8_t *blockPtr;
     uint32_t blockId;
     uint32_t Residue;
 }LogBlock;
 
 
-typedef struct LogSegment
+class LogSegment
 {
-    LogBlock logBlocks[MAX];
-    StoreStats storeStats;
-    TableStats tableStats;
-    uint32_t numPages;  // change name?
-    uint32_t usingPage;
+private:
+    /* data */
+public:
+    LogBlock *logBlocks[MAX];   // TODO: use SHM_MAX_PAGES - 1 here later
+    StoreStats *storeStats;
+    TableStats *tableStats;
+    uint32_t blockNum;
+    uint32_t usingBlock;
     uint32_t offset;
-}LogSegment;
+    uint32_t round;
 
+    LogSegment(/* args */);
+    ~LogSegment();
+
+    int64_t allocItem(uint64_t item_size);
+};
 
 
 class Log
 {
 private:
-public:
-    LogSegment logSegment[MAX];
-    uint16_t totalNumPage; // change name?
     uint16_t resizingPointer;
+
+
+public:
+    LogSegment *logSegments[THREAD_NUM];
+    uint64_t totalBlockNum;
+    uint16_t totalSegmentNum;
 
     Log(/* args */);
     ~Log();
-    temp shrink();
-    temp expand();
+
+    void shrink(uint64_t numBlockToShrink);
+    void expand(TableBlock **tableblocksToMove, uint64_t numBlockToExpand, size_t blockSize);
     temp locateItem();
-    temp allocItem();
+
+    uint16_t get_next_resize_segment_id(int expandOrShrink); // expand: 0  shrink: 1
+    void set_next_resize_segment_id(int expandOrShrink);
 };
 
-Log::Log(/* args */)
-{
-}
 
-Log::~Log()
-{
-}
 
 
 
